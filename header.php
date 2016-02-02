09dda478f528d1823941a93e8f0f0b6b72508a45
Mikail Coban <cobanmikail@icloud.com>
2016-02-02 22:18:38 +0100

    asdf

---
+++ header.php
@@ -0,0 +1,9 @@
+<header>
+	<a href="home.php"><img id="logo" src="foldericon.png" alt="Rhapsody"></a>
+    <div class="headerSocial">
+        <a href="https://twitter.com/RHAPSODY_010" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false" data-dnt="true">Follow @RHAPSODY_010</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+        <a href="https://www.instagram.com/rhapsody_010/?ref=badge" class="ig-b- ig-b-24"><img src="//badges.instagram.com/static/images/ig-badge-24.png" alt="Instagram" /></a>
+    </div>
+    <div id="headerSlogan"><img id="slogan" src="rhapsody_slogan.png" alt="Rhapsody Slogan"></div>
+</header>
