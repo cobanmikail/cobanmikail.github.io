f89e318a0e08fc80d9befd89885c8852213d6067
Mikail Coban <cobanmikail@icloud.com>
2016-02-02 21:02:20 +0100

    sfd

--- navbar.php
+++ navbar.php
@@ -1,8 +1,10 @@
 <html>
+<nav>
 <ul>
   <li><a href="default.deliverables.html">Home</a></li>
   <li><a href="deliverables.html">Deliverables</a></li>
   <li><a href="dump.html">Dump</a></li>
   <li><a href="about.html">About</a></li>
 </ul>
+</nav>
 </html>
