abc1b8bb9087cff6509aeb23c2461ee5be1f77ba
Mikail Coban <cobanmikail@icloud.com>
2016-02-02 23:08:43 +0100

    safd

--- index.php
+++ index.php
@@ -2,18 +2,17 @@
 <html>
 <head>
 <title>Portfolio Site MMIO Mikail Çoban</title>
-<div class="center">
-		<h1> Portfoliosite MMIO </h1>
-		<h2> Interactieontwerp en Multimediaontwerpproject </h2>
-		<h3> Mikail Çoban 10983783 </h3>
-	</div>
-<a href="index.html"><img id="logo" src="foldericon.png" alt="Rhapsody"></a>
 <link rel="stylesheet" type="text/css" href="portfolio.css">
 <link href='https://fonts.googleapis.com/css?family=Roboto:300,700,900' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
-	
+	<?php include("header.php") ?>
+	<div class="center">
+		<h1> Portfoliosite MMIO </h1>
+		<h2> Interactieontwerp en Multimediaontwerpproject </h2>
+		<h3> Mikail Çoban 10983783 </h3>
+	</div>
 
 
 
